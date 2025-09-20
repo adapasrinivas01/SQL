42e03ff22a30b45c57673d524859c7bd5bb84467
Naga Durga Srinivas Adapa <adapasrinivas2004@gmail.com>
2025-09-20 08:08:00 +0530

    Create insert_new_customer_record.sql

---
+++ insert_new_customer_record.sql
@@ -0,0 +1,5 @@
+insert into Customer
+(customerid, fullname, email, city, registrationdate, status) 
+values (5, 'John Doe', 'john.doe@example.com', 'New York', '2024-01-01', 'Active');
+
+select * from Customer;
