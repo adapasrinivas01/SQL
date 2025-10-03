b34658882153136e5bb40d5bfd15894ad8545484
Naga Durga Srinivas Adapa <adapasrinivas2004@gmail.com>
2025-10-03 10:27:46 +0530

    Create Update_the_price_of_cars_from_2021_to_25,000.00,_and_for_2020_cars_to_18,000.00.sql

---
+++ Update_the_price_of_cars_from_2021_to_25,000.00,_and_for_2020_cars_to_18,000.00.sql
@@ -0,0 +1,13 @@
+update cars
+set price=25000
+where yearofmanufacture=2021;
+
+update cars
+set price=18000
+where yearofmanufacture=2020;
+
+select * from cars
+where yearofmanufacture=2021;
+
+select * from cars
+where yearofmanufacture=2020;
